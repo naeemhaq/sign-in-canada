316edc68187437ae79b1cd8b2dfa0df61d23e393
naeemhaq <10832647+naeemhaq@users.noreply.github.com>
2020-01-27 22:30:12 -0500

    replacing the ipaddress in the setup file.

--- gluu-az-template/install_apache.sh
+++ gluu-az-template/install_apache.sh
@@ -37,4 +37,5 @@ apt-get install gluu-server
 cd /install/community-edition-setup
 echo pwd
 wget https://raw.githubusercontent.com/naeemhaq/sign-in-canada/master/gluu-az-template/template2/setup.properties
+sed -i "s/10.1.0.5/$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)/g" setup.properties
 ./setup.py -psn -f setup.properties 
