e018d8819fa31d58bdc3847ea1118368e1d9cc59
naeemu <naeem_ulhaq@trendmicro.com>
2020-03-01 18:06:40 -0500

    updating setup file.

--- gluu-az-template/install_apache.sh
+++ gluu-az-template/install_apache.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
 # isntall gluu server 
-echo "gluu server install begins"
+echo "setting up repos for gluu"
 wget https://repo.gluu.org/rhel/Gluu-rhel7.repo -O /etc/yum.repos.d/Gluu.repo
 wget https://repo.gluu.org/rhel/RPM-GPG-KEY-GLUU -O /etc/pki/rpm-gpg/RPM-GPG-KEY-GLUU
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-GLUU
 yum clean all
-yum install -y gluu-server
+
 # update hosts file with hostname and IP addresses
 echo "updating hosts file with hostname and IP addresses"
 
 ip=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2017-08-01&format=text")
- hostname=gluuserver-cc-01.canadacentral.cloudapp.azure.com
- privateIP=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2017-08-01&format=text")
- sed -i.bkp "$ a $ip $hostname" /etc/hosts
- sed -i.bkp "$ a $privateIP $hostname" /etc/hosts
+hostname=gluuserver-cc-01.canadacentral.cloudapp.azure.com
+privateIP=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2017-08-01&format=text")
+sed -i.bkp "$ a $ip $hostname" /etc/hosts
+sed -i.bkp "$ a $privateIP $hostname" /etc/hosts
 
- 
+echo "gluu server install begins"
+yum install -y gluu-server
+
+echo "downloading setup.py and updating properties file"
+cd /opt/gluu-server/install/community-edition-setup
+wget https://raw.githubusercontent.com/naeemhaq/sign-in-canada/master/gluu-az-template/template2/setup.properties
+sed -i "s/10.1.0.5/$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)/g" setup.properties
+
+cd
+
 echo "enabling gluu server and logging into container"
 /sbin/gluu-serverd enable
 /sbin/gluu-serverd start
 /sbin/gluu-serverd login
 cd /install/community-edition-setup
-echo pwd
-wget https://raw.githubusercontent.com/naeemhaq/sign-in-canada/master/gluu-az-template/template2/setup.properties
-sed -i "s/10.1.0.5/$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)/g" setup.properties
 ./setup.py -psn -f setup.properties 
-
