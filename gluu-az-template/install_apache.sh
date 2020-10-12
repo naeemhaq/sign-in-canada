4f0a0bc1c9f41b31c6b098b1b0fde084405d7850
Naeem UlHaq <naeem.ulhaq@nqtech.ca>
2020-10-12 15:34:24 -0400

    updated install scripts and dnsprefix

--- gluu-az-template/install_apache.sh
+++ gluu-az-template/install_apache.sh
@@ -10,13 +10,27 @@ yum clean all
 # update hosts file with hostname and IP addresses
 echo "updating hosts file with hostname and IP addresses"
 
+zayn=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/name?api-version=2017-08-01&format=text")
+hostname="${zayn}.canadacentral.cloudapp.azure.com"
 ip=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2017-08-01&format=text")
-hostname=gluuserver-cc-01.canadacentral.cloudapp.azure.com
 privateIP=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2017-08-01&format=text")
 sed -i.bkp "$ a $ip $hostname" /etc/hosts
 sed -i.bkp "$ a $privateIP $hostname" /etc/hosts
 echo > /etc/hostname
 echo $hostname > /etc/hostname
+
+# certbot ini file. 
+echo -e "# Use a 4096 bit RSA key instead of 2048.\nrsa-key-size = 4096\n\n# Set email and domains.\nemail = info@nqtech.ca\ndomains = ${hostname}\n\n\n# Text interface.\ntext = True\n# No prompts.\nnon-interactive = True\n# Suppress the Terms of Service agreement interaction.\nagree-tos = True\n\n# Use the webroot authenticator.\nauthenticator = webroot\nwebroot-path = /var/www/html" >
+
+echo "install certbot"
+yum -y install epel-release
+yum -y install snapd
+systemctl enable --now snapd.socket
+ln -s /var/lib/snapd/snap /snap
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install certbot python2-certbot-apache
+
+sudo certbot certonly --standalone
 
 echo "gluu server install begins"
 #yum install -y gluu-server
@@ -33,17 +47,6 @@ echo "enabling gluu server and logging into container"
 echo "downloading SIC tarball"
 wget https://sicqa.blob.core.windows.net/staging/SIC-AP-0.0.31.tgz
 tar -xvf SIC-AP-0.0.31.tgz
-
-#echo "downloading couchbase"
-#mkdir /opt/gluu-server/opt/dist/couchbase
-#wget https://nqtech.blob.core.windows.net/gluustaging/couchbase-server-enterprise-6.5.0-centos7.x86_64.rpm
-#mv /var/lib/waagent/custom-script/download/0/couchbase-server-enterprise-6.5.0-centos7.x86_64.rpm /opt/gluu-server/opt/dist/couchbase/
-#echo "done!!"
-
-echo "install certbot"
-yum -y install yum-utils
-yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum -y install certbot python2-certbot-apache
 
 exit
 
--- gluu-az-template/install_couchbase.sh
+++ gluu-az-template/install_couchbase.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# update hosts file with hostname and IP addresses
+echo "updating hosts file with hostname and IP addresses"
+
+zayn=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/name?api-version=2017-08-01&format=text")
+hostname="${zayn}.canadacentral.cloudapp.azure.com"
+ip=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2017-08-01&format=text")
+privateIP=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2017-08-01&format=text")
+sed -i.bkp "$ a $ip $hostname" /etc/hosts
+sed -i.bkp "$ a $privateIP $hostname" /etc/hosts
+echo > /etc/hostname
+echo $hostname > /etc/hostname
+
 echo "pre-installation steps"
 mkdir /etc/tuned/no-thp
 echo -e "[main]\ninclude=virtual-guest\n[vm]\ntransparent_hugepages=never" > /etc/tuned/no-thp/tuned.conf
@@ -9,6 +21,52 @@ cp -p /etc/sysctl.conf /etc/sysctl.conf.`date +%Y%m%d-%H:%M`
 sh -c 'echo "" >> /etc/sysctl.conf'
 sh -c 'echo "#Set swappiness to 0 to avoid swapping" >> /etc/sysctl.conf'
 sh -c 'echo "vm.swappiness = 0" >> /etc/sysctl.conf'
+
+set -o nounset
+set -o errexit
+ 
+# May or may not have HOME set, and this drops stuff into ~/.local.
+export HOME="/root"
+export PATH="${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+ 
+# No package install yet.
+wget https://dl.eff.org/certbot-auto
+chmod a+x certbot-auto
+mv certbot-auto /usr/local/bin
+ 
+# Install the dependencies.
+certbot-auto --noninteractive --os-packages-only
+ 
+# Set up config file.
+mkdir -p /etc/letsencrypt
+cat > /etc/letsencrypt/cli.ini <<EOF
+# Uncomment to use the staging/testing server - avoids rate limiting.
+# server = https://acme-staging.api.letsencrypt.org/directory
+ 
+# Use a 4096 bit RSA key instead of 2048.
+rsa-key-size = 4096
+ 
+# Set email and domains.
+email = info@nqteh.ca
+domains = ${hostname}
+ 
+# Text interface.
+text = True
+# No prompts.
+non-interactive = True
+# Suppress the Terms of Service agreement interaction.
+agree-tos = True
+ 
+# Use the webroot authenticator.
+authenticator = webroot
+webroot-path = /var/www/html
+EOF
+ 
+# Obtain cert.
+certbot-auto certonly
+
+#echo "creating certbot ini file"
+#echo -e "# Use a 4096 bit RSA key instead of 2048.\nrsa-key-size = 4096\n\n# Set email and domains.\nemail = info@nqtech.ca\ndomains = ${hostname}\n\n\n# Text interface.\ntext = True\n# No prompts.\nnon-interactive = True\n# Suppress the Terms of Service agreement interaction.\nagree-tos = True\n\n# Use the webroot authenticator.\nauthenticator = webroot\nwebroot-path = /var/www/html" >
 
 echo "install couchbase"  
 curl -O https://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-x86_64.rpm
