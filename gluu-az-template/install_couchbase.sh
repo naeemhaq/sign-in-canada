685750acbdc179f83510d406f6531e0b55f746e1
Naeem UL-Haq <naeemu@macbook-pro.lan>
2020-10-18 20:42:14 -0400

    removed certbot

--- gluu-az-template/install_couchbase.sh
+++ gluu-az-template/install_couchbase.sh
@@ -29,64 +29,6 @@ sh -c 'echo "" >> /etc/sysctl.conf'
 sh -c 'echo "#Set swappiness to 0 to avoid swapping" >> /etc/sysctl.conf'
 sh -c 'echo "vm.swappiness = 0" >> /etc/sysctl.conf'
 
-echo "setting up certbot"
-set -o nounset
-set -o errexit
-
-echo "dnf install"
-dnf -y install epel-release
-
-echo "dnf upgrade"
-#dnf -y upgrade
-
-echo "yum snapd"
-yum -y install snapd
-
-echo "enable snapd.socket"
-systemctl enable --now snapd.socket
-
-echo "setup link to snap"
-ln -s /var/lib/snapd/snap /snap
-
-systemctl restart snapd.seeded.service
-
-echo "check snap version with core and refresh"
-snap install core; sudo snap refresh core
-
-echo "install certbot"
-snap install --classic certbot
-
-echo "setup link"
-ln -s /snap/bin/certbot /usr/bin/certbot
- 
-echo "Set up cert config file."
-mkdir -p /etc/letsencrypt
-cat > /etc/letsencrypt/cli.ini <<EOF
-# Uncomment to use the staging/testing server - avoids rate limiting.
-server = https://acme-staging.api.letsencrypt.org/directory
- 
-# Use a 4096 bit RSA key instead of 2048.
-rsa-key-size = 4096
- 
-# Set email and domains.
-email = info@nqteh.ca
-domains = ${hostname}
- 
-# Text interface.
-text = True
-# No prompts.
-non-interactive = True
-# Suppress the Terms of Service agreement interaction.
-agree-tos = True
- 
-# Use the webroot authenticator.
-#authenticator = webroot
-#webroot-path = /var/www/html
-EOF
-
-echo "run certbot"
-certbot certonly --standalone
-
 echo "install couchbase"  
 curl -O https://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-x86_64.rpm
 sudo rpm -i ./couchbase-release-1.0-x86_64.rpm
