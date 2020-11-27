f5f68ec2e6269e91e6da415ef8249210a0de31bb
Naeem UlHaq <naeem.ulhaq@nqtech.ca>
2020-11-27 15:29:28 -0500

    updated tarball versions

--- gluu-az-template/install_apache.sh
+++ gluu-az-template/install_apache.sh
@@ -94,10 +94,10 @@ fi
 sed -i "/^loadData=True/ s/.*/loadData=False/g" setup.properties
 
 echo "downloading SIC tarball"
-wget https://gluuccrgdiag.blob.core.windows.net/gluu/SIC-Admintools-0.0.20.tgz
-wget https://gluuccrgdiag.blob.core.windows.net/gluu/SIC-AP-0.0.198.tgz
+wget https://gluuccrgdiag.blob.core.windows.net/gluu/SIC-Admintools-0.0.22.tgz
+wget https://gluuccrgdiag.blob.core.windows.net/gluu/SIC-AP-0.0.201.tgz
 
-tar -xvf SIC-Admintools-0.0.20.tgz
+tar -xvf SIC-Admintools-0.0.22.tgz
 
 pwd 
 ls -al 
@@ -110,4 +110,4 @@ KEYVAULT_URL=${KEYVAULT}
 METADATA_URL=https://sicqa.blob.core.windows.net/saml/SIC-Nonprod-signed.xml
 EOF
 
-./install.sh SIC-AP-0.0.198
+./install.sh SIC-AP-0.0.201
