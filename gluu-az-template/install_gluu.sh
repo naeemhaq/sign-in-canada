8779499662f8599716fdf7ab048e1e006c5f5678
Naeem UL-Haq <naeemu@MacBook-Pro.local>
2021-01-17 18:19:50 -0500

    updating var grups test

--- gluu-az-template/install_gluu.sh
+++ gluu-az-template/install_gluu.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Please specify Couchbase IP"
+    echo "./install.sh 10.0.1.4"
+    exit
+fi
 
-# isntall gluu server 
-echo "setting up repos for gluu"
-echo "deb https://repo.gluu.org/ubuntu/ bionic main" > /etc/apt/sources.list.d/gluu-repo.list
-curl https://repo.gluu.org/ubuntu/gluu-apt.key | apt-key add -
-apt-get update
+TARBALL="SIC-AP-0.0.223"
+KEYVAULT="https://kv-sic-dev-00.vault.azure.net"
 
-# update hosts file with hostname and IP addresses
-echo "updating hosts file with hostname and IP addresses"
+# set the hostname
+echo "Setting the hostname"
+zayn=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/name?api-version=2017-08-01&format=text")
+hostname ${zayn}.id.alpha.canada.ca
 
-ip=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2017-08-01&format=text")
-hostname=gluuserver-cc-01.canadacentral.cloudapp.azure.com
-privateIP=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2017-08-01&format=text")
-sed -i.bkp "$ a $ip $hostname" /etc/hosts
-sed -i.bkp "$ a $privateIP $hostname" /etc/hosts
-echo > /etc/hostname
-echo $hostname > /etc/hostname
+# Install epel and jq
+yum clean all
+yum install -y epel-release
+yum install -y jq
 
-echo "gluu server install begins"
-apt-get install gluu-server
+# isntall gluu GPG key TODO: Use alternate supply path
+echo "Setting up Gluuu GPG key"
+wget https://repo.gluu.org/rhel/RPM-GPG-KEY-GLUU -O /etc/pki/rpm-gpg/RPM-GPG-KEY-GLUU
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-GLUU
 
-echo "enabling gluu server and logging into container"
-/sbin/gluu-serverd enable
-/sbin/gluu-serverd start
-#/sbin/gluu-serverd login
-#cd /install/community-edition-setup
-#./setup.py -psn -f setup.properties 
 
+# TODO: Have the Custom Script Extension do this
+echo "Downloading Software"
+wget https://repo.gluu.org/centos/7/gluu-server-4.2.2-centos7.x86_64.rpm
+wget https://raw.githubusercontent.com/sign-in-canada/Admin-Tools/develop/software/install.sh
+
+echo "Running SIC setup"
+export STAGING_URL=https://sicqa.blob.core.windows.net/staging
+export KEYVAULT_URL=${KEYVAULT}
+export METADATA_URL=https://sicqa.blob.core.windows.net/saml/SIC-Nonprod-signed.xml
+export CB_HOSTS=${1}
+
+sh ./install.sh ${TARBALL}
